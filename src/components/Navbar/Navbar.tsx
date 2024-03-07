248a19308ad21f5d7c96a4052418bc847f38c31e
Avetis Muradyan <avetismu@gmail.com>
2024-03-07 18:13:02 +0800

    Navbar and Drawer added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = () => {
 
     return (
-        <AppBar position="static">
+        <AppBar position="fixed" sx={{zIndex: (theme) => theme.zIndex.drawer + 1 }}>
             <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} align="left">
                     OceanCRM
